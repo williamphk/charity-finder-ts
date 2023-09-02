5fc8ef2538c1a4093fa5b7d62cb11b88e187ddd0
William Poon <willphk@outlook.com>
2023-09-02 00:23:29 -0400

    Updated SearchComponent

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -1,35 +1,108 @@
 import { useState } from "react";
 
+import "./SearchComponent.css";
+
+const terms = [
+  "aapi-led",
+  "adoption",
+  "afghanistan",
+  "animals",
+  "art",
+  "athletics",
+  "autism",
+  "black-led",
+  "buddhism",
+  "cancer",
+  "cats",
+  "christianity",
+  "climate",
+  "conservation",
+  "coronavirus",
+  "culture",
+  "dance",
+  "disabilities",
+  "disease",
+  "dogs",
+  "education",
+  "environment",
+  "filmandtv",
+  "food-security",
+  "freepress",
+  "gender-equality",
+  "health",
+  "hinduism",
+  "housing",
+  "humans",
+  "hurricane-ian",
+  "immigrants",
+  "indigenous-led",
+  "indigenous-peoples",
+  "islam",
+  "judaism",
+  "justice",
+  "latine-led",
+  "legal",
+  "lgbt",
+  "libraries",
+  "mental-health",
+  "museums",
+  "music",
+  "oceans",
+  "parks",
+  "poverty",
+  "racial-justice",
+  "radio",
+  "refugees",
+  "religion",
+  "research",
+  "science",
+  "seniors",
+  "space",
+  "theater",
+  "transgender",
+  "ukraine",
+  "veterans",
+  "votingrights",
+  "water",
+  "wildfires",
+  "wildlife",
+  "women-led",
+  "womens-health",
+  "youth",
+];
+
 interface SearchComponentProps {}
 
 const SearchComponent: React.FC<SearchComponentProps> = ({}) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<string[]>([]);
 
   const handleSearchTermChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = () => {
-    searchCharities(searchTerm);
-    console.log(import.meta.env.VITE_API_KEY);
-  };
-
-  const searchCharities = (searchTerm: string) => {
-    // fetch(
-    //   `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.KEY}&app_key=${process.env.REACT_APP_CHARITY_NAVIGATOR_APP_KEY}&search=${searchTerm}`
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //   });
+    const searchQuery = event.target.value;
+    setSearchTerm(searchQuery);
+    if (searchQuery.length === 0) {
+      setSearchResults([]);
+      return;
+    }
+    const results = terms
+      .filter((term) => term.toLowerCase().includes(searchQuery.toLowerCase()))
+      .slice(0, 10);
+    setSearchResults(results);
   };
 
   return (
     <div>
       <input type="text" value={searchTerm} onChange={handleSearchTermChange} />
-      <button onClick={handleSearch}>Search</button>
+      <button>Search</button>
+      <div className="autocomplete-results">
+        {searchResults.map((result, index) => (
+          <div key={index} className="autocomplete-result">
+            {result}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
