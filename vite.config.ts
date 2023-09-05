0a901f67ef0adfd521668e5850d36184ea77d8c4
William Poon <willphk@outlook.com>
2023-09-04 20:44:07 -0400

    Updated Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+    ENV_KEY: process.env.VITE_API_KEY,
+  },
+});
